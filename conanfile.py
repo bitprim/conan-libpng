edacd01f419277665de5141905ecbb1d32c58953
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-02-08 08:38:20 -0300

    disable tests

--- conanfile.py
+++ conanfile.py
@@ -72,7 +72,7 @@ class LibpngConan(ConanFile):
             tools.replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, 'COMMAND "${CMAKE_COMMAND}" -E copy_if_different $<TARGET_LINKER_FILE_NAME:${S_TARGET}> $<TARGET_LINKER_FILE_DIR:${S_TARGET}>/${DEST_FILE}',
                                   'COMMAND "${CMAKE_COMMAND}" -E copy_if_different $<TARGET_LINKER_FILE_DIR:${S_TARGET}>/$<TARGET_LINKER_FILE_NAME:${S_TARGET}> $<TARGET_LINKER_FILE_DIR:${S_TARGET}>/${DEST_FILE}')
         cmake = CMake(self)
-        cmake.definitions["PNG_TESTS"] = "ON"
+        cmake.definitions["PNG_TESTS"] = "OFF"
         cmake.definitions["PNG_SHARED"] = self.is_shared
         cmake.definitions["PNG_STATIC"] = not self.is_shared
         cmake.definitions["PNG_DEBUG"] = "OFF" if self.settings.build_type == "Release" else "ON"
